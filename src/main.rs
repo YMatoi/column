9dc5ac16e1318b9414727d9dccda0de59a850c12
YMatoi <YMatoi@users.noreply.github.com>
2020-03-29 18:52:13 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use std::io::{self, BufRead, BufReader, BufWriter};
 use std::io::Write;
 use std::env;
 
+fn columns(line: &String, column_numbers: &Vec<usize>) -> String {
+    column_numbers.iter()
+        .filter_map( |n| line.split_whitespace().nth(*n))
+        .collect::<Vec<&str>>()
+        .join("\t")
+}
+
 fn main() {
     let args: Vec<usize> = env::args()
         .map(|arg| arg.parse::<usize>())
@@ -17,11 +24,7 @@ fn main() {
     let mut buf = String::new();
 
     while reader.read_line(&mut buf).unwrap() > 0 {
-        let a: Vec<&str> = args.iter()
-            .filter_map(|n| buf.split_whitespace().nth(*n))
-            .collect();
-        let message = a.join("\t"); 
-        writeln!(writer, "{}",message).unwrap();
+        writeln!(writer, "{}", columns(&buf, &args)).unwrap();
         buf.clear();
     }
 }
