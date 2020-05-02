6d7f8588afc8a065de75e46f55368e1db38e42d0
YMatoi <YMatoi@users.noreply.github.com>
2020-05-02 22:30:56 +0900

    add separator

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use std::io::{self, BufRead, BufReader, BufWriter};
-use std::io::Write;
 use std::env;
+use std::io::Write;
+use std::io::{self, BufRead, BufReader, BufWriter};
 
 #[macro_use]
 extern crate clap;
 use clap::Arg;
 
-fn columns(line: &String, column_numbers: &Vec<usize>)-> String {
-    column_numbers.iter()
-        .filter_map( |n| line.split_whitespace().nth(*n))
+fn columns(line: &String, column_numbers: &Vec<usize>, separator: Option<&str>) -> String {
+    let split = |n: usize| match separator {
+        Some(s) => line.split(s).nth(n),
+        None => line.split_whitespace().nth(n),
+    };
+
+    column_numbers
+        .iter()
+        .filter_map(|n| split(*n))
         .collect::<Vec<&str>>()
         .join("\t")
 }
@@ -30,19 +36,25 @@ fn main_loop(func: &dyn Fn(&String) -> String) {
 
 fn main() {
     let matches = app_from_crate!()
-        .arg(Arg::with_name("columns")
-             .multiple(true)
-             .takes_value(true)
-             .required(true)
+        .arg(Arg::with_name("separator").short("s").takes_value(true))
+        .arg(
+            Arg::with_name("columns")
+                .multiple(true)
+                .takes_value(true)
+                .required(true),
         )
         .get_matches();
 
-    let args: Vec<usize> = matches.values_of("columns").unwrap()
+    let separator: Option<&str> = matches.value_of("separator");
+
+    let args: Vec<usize> = matches
+        .values_of("columns")
+        .unwrap()
         .map(|arg| arg.parse::<usize>())
         .filter_map(Result::ok)
         .collect();
 
-    let func = |s: &String| columns(s, &args);
+    let func = |s: &String| columns(s, &args, separator);
 
     main_loop(&func);
 }
